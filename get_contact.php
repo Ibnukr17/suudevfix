60fb64c698f70e3765bc2d2c0d36a5bac1047805
Hilda <hildakh07@gmail.com>
2022-06-29 20:54:19 +0700

    update get contact

--- get_contact.php
+++ get_contact.php
@@ -1,14 +1,17 @@
 <?php
-	include 'dbconnect.php';
- 
-	if(isset($_POST['send_contact'])){
-        mysqli_query($connect, "insert into contact set
+include 'dbconnect.php';
+
+if (isset($_POST['send_contact'])) {
+    mysqli_query($connect, "insert into contact set
         firstname = '$_POST[firstname]',
         lastname = '$_POST[lastname]',
         email = '$_POST[email]',
         phone = '$_POST[phone]',
         message = '$_POST[message]'");
 
-        echo "Message already send!";
-	}
-?>
+    echo '<script type ="text/JavaScript">';
+    echo 'alert("Selamat Belajar PHP")';
+    echo '</script>';
+    echo "<meta http-equiv='refresh' content='0; url=index.php?p=contact'>";
+    // echo "Message already send!";
+}
